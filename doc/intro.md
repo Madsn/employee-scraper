c4ac0bf9350dc4c51f4bf92f1b5fdb010009e7ca
Mikkel Madsen <m@madsn.net>
2014-03-19 20:17:04 +0100

    Eclipse project initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to employee-scraper
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject employee-scraper "0.1.0-SNAPSHOT"
+  :description "Tool for scraping employee info from my employers sharepoint"
+  :url "None"
+  :dependencies [[org.clojure/clojure "1.5.1"]])
