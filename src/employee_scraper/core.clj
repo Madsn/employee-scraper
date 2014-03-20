f6f588632cd4b65e76407749d277e08ba6ee60ae
Mikkel Madsen <m@madsn.net>
2014-03-20 09:11:59 +0100

    save initials and names to csv file

--- src/employee_scraper/core.clj
+++ src/employee_scraper/core.clj
@@ -9,17 +9,24 @@
   (find-element {:id "PageLinkNext"}))
 
 (defn person-finder []
-  (core/find-elements *driver* {:class "ms-peopleux-userImg"}))
+  (core/find-elements *driver* {:id "NameFieldLink"}))
 
+(defn get-match-str [srcstr]
+  (re-find #"SYSTEMATICGROUP%5C\S+" srcstr))
+  
 (defn get-initials [elem]
-  (subs (re-find #"Pictures/\S+(?=_)" (attribute elem :src)) 9))
+  (let [srcstr (attribute elem :href)]
+	(subs (get-match-str srcstr) 18)))
   
 (defn get-name [elem]
-  (clojure.string/trim (attribute elem :alt)))
+  (let [name-elem (attribute elem :title)]
+    (clojure.string/trim name-elem)))
   
 (defn print-names []
   (doseq [pelem (person-finder)]
-     (println (str (get-initials pelem) " - " (get-name pelem)))))
+     (spit "initials-and-names-from-sharepoint.txt" 
+	       (str (get-initials pelem) "," (get-name pelem) "\n") 
+		   :append true)))
 
 (defn process-page []
   (let [nextbtn (get-next-btn)]
